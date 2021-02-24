57533a8d8679fcbfc1de3689e9eda40c4aa96aed
Pablo Sarasua <p.sarasua@hotmail.com>
2021-02-23 22:05:35 -0300

    Se Agrega CountDown

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+//TimerCountDown
+$('#example').countdown({
+    date: '12/25/2021 00:00:00',
+    offset: -3,
+    day: 'Day',
+    days: 'Dias',
+    hour: 'Hora',
+    hours: 'Horas',
+    minute: 'Minuto',
+    minutes: 'Minutos',
+    second: 'Segundo',
+    seconds: 'Segundos'
+});
+
+//PreLoader
 function counter() {
     var count = setInterval(function() {
         var c = parseInt($('.counter').text());
