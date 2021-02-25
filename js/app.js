57261d320c87bc5ee3a1cd0e67d2181473f89dff
Pablo Sarasua <p.sarasua@hotmail.com>
2021-02-25 20:56:37 -0300

    implementado JavaScript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,121 @@
+//Variables
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaProductos = document.querySelector("#lista-productos");
+let articulosCarrito = [];
+
+cargarEventListener();
+
+function cargarEventListener() {
+    //Cuando Agregas un Producto presionando Comprar
+    listaProductos.addEventListener("click", agregarProducto);
+}
+//Elimina productos
+carrito.addEventListener('click', eliminarProducto);
+
+//Vaciar el Carrito Btn
+vaciarCarritoBtn.addEventListener('click', () => {
+    articulosCarrito = []; //Resetear el arreglo
+    limpiarHTML(); //Redibujamos el HTML
+})
+
+//Funciones
+function agregarProducto(e) {
+    e.preventDefault();
+    if (e.target.classList.contains("agregar-carrito")) {
+        const button = e.target;
+        const item = button.closest('.card');
+
+        leerDatosProducto(item);
+    }
+}
+
+//Eliminar Productoa
+function eliminarProducto(e) {
+    //Me aseguro de presionar donde la clase borrar-producto
+    if (e.target.classList.contains('borrar-producto')) {
+        //Guardo el valor del atributo data-id
+        const productId = e.target.getAttribute('data-id');
+
+        //Eliminar del Arreglo por el data-id
+        articulosCarrito = articulosCarrito.filter(producto => producto.id !== productId);
+        //Vuelvo a dibujar el carrito con el nuevo arreglo
+        carritoHTML();
+
+    }
+}
+
+//Lee los datos
+function leerDatosProducto(producto) {
+    //Creo objeto con la informacion del producto seleccionado
+    const infoProducto = {
+        imagen: producto.querySelector('img').src,
+        descripcion: producto.querySelector('.card-title').textContent,
+        precio: producto.querySelector('.item-price').textContent,
+        id: producto.querySelector('input').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    //Reviso si existe ya el producto en el carrito
+    const existe = articulosCarrito.some(producto => producto.id === infoProducto.id);
+    if (existe) {
+        //Actualizo la cantidad
+        const productos = articulosCarrito.map(producto => {
+            if (producto.id === infoProducto.id) {
+                producto.cantidad++;
+                return producto; //Retorna el objeto actualizado
+            } else {
+                return producto; //Retorna los objetos no duplicados
+            }
+        });
+        articulosCarrito = [...productos];
+
+    } else {
+
+        //Agrega elementos al arreglo de carrito
+        articulosCarrito = [...articulosCarrito, infoProducto];
+    }
+    carritoHTML();
+}
+
+//Muestra el carrito en el HTML
+function carritoHTML() {
+
+    //Limpiar el HTML
+    limpiarHTML();
+
+    articulosCarrito.forEach(producto => {
+        const { imagen, descripcion, precio, cantidad, id } = producto;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            <img src="${imagen}" width="100">
+        </td>
+        <td>
+        ${descripcion}
+        </td>
+        <td>
+        ${precio}
+        </td>
+        <td>
+        ${cantidad}
+        </td>
+        <td>
+        <a href="#" class="btn btn-danger borrar-producto" data-id="${id}"> x</a>
+        </td>
+      `;
+        //Agrega el row al tbody
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+//Eliminar Productos del tbody
+function limpiarHTML() {
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
 //TimerCountDown
 $('#example').countdown({
     date: '12/25/2021 00:00:00',
